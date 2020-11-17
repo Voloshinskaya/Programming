fb85f35af69f6923c075709933bcad432f0dad34
Your Name <you@example.com>
2020-11-17 20:05:43 +0300

    Работа на практике

---
+++ Practice/24/С++/24/24/24.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::ifstream in_file("in.json");
+    json j2 = json::array();
+    if (in_file.is_open());
+    {
+        std::cout << "Файл открыт:"<< std::endl;
+        json j;
+        in_file >> j;
+        std::cout << j;
+    }
+    else { cout << "Ошибка! Не удалось открыть файл!" << std::endl; }
+    }
+
+}
